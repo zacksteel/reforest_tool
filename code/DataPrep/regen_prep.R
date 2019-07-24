fbd4a832d1b18c5f6b23a247475d7ff18d97ba83
Zachary L Steel <zlsteel@ucdavis.edu>
2019-07-24 15:51:51 -0700

    messing with regen more

--- code/DataPrep/regen_prep.R
+++ code/DataPrep/regen_prep.R
@@ -10,12 +10,12 @@ regen_prep <- function() {
     dplyr::select(plot, forest, aoi, loc, treated)
   
   ## Species x-walk
-  sdf <- data.frame(code = c("PIP", "QUKE", "CADE", "ABCO", "PILA", "QUCH", 
+  sdf <- data.frame(code = c("PIPO", "QUKE", "CADE", "ABCO", "PILA", "QUCH", 
                              "PIJE", "SALIX", "PSME", "CONU"),
                     species = c("Ponderosa pine", "Black oak", "Incense cedar",
                                 "White fir", "Sugar pine", "Canyon live oak",
                                 "Jeffrey pine", "Willow sp.", "Douglas fir",
-                                " "))
+                                "Dogwood"))
   
   ## First calculate plot area as a proportion of a hectare
   plot_area <- pi*12.6^2 / 10000 #12.6m radius; returns hectares
@@ -24,6 +24,7 @@ regen_prep <- function() {
   d2 <- dplyr::select(d, plot = Plot.Name, code = SaplingSpeciesCode, status = Status,
                count = Count) %>%
     filter(status == "L") %>%
+    filter(code %in% c("PIPO", "PILA", "CADE", "ABCO", "QUKE", "QUCH")) %>%
     merge(sdf) %>%
     group_by(plot, code, species) %>%
     summarise(spha = round(sum(count) / plot_area, 0)) %>%
